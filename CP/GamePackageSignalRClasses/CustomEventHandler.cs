1da4a83af44ca8469d3f1a4cb0088319925eb00b
Andy <musictopia2@hotmail.com>
2019-09-21 20:38:11 -0700

    New Signal R Class

---
+++ CP/GamePackageSignalRClasses/CustomEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using BasicGameFramework.NetworkingClasses.Data;
+namespace GamePackageSignalRClasses
+{
+    public class CustomEventHandler
+    {
+        public CustomEventHandler(EnumNetworkCategory category, string message)
+        {
+            Category = category;
+            Message = message;
+        }
+        public CustomEventHandler(EnumNetworkCategory category)
+        {
+            Category = category;
+            Message = "";
+        }
+        public string Message { get; } = "";
+        public EnumNetworkCategory Category { get; }
+    }
+}
