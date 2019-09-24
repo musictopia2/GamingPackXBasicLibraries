1e1e87d130f3a4612b76bf7ed7cd5abe50b0ed71
Andy <musictopia2@hotmail.com>
2019-09-24 09:17:00 -0700

    New Screen Interface For Xamarin Forms

---
+++ CP/BasicGameFramework/CommonInterfaces/IScreen.cs
@@ -0,0 +1,7 @@
+﻿namespace BasicGameFramework.CommonInterfaces
+{
+    public interface IScreen
+    {
+        void CalculateScreens(); //decided to make xamarin forms implement it after all.  if it decides to ignore in test mode, they have that right too.
+    }
+}
