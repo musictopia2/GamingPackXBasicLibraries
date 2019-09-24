4fda79647cdf118d8bf3f2470805d10e7d880cde
Andy <musictopia2@hotmail.com>
2019-09-24 08:10:35 -0700

    Implement suggested start up class for single player games

---
+++ CP/BasicGameFramework/StandardImplementations/CrossPlatform/MiscClasses/SinglePlayerStartUpClass.cs
@@ -0,0 +1,19 @@
+﻿using BasicGameFramework.BasicGameDataClasses;
+using BasicGameFramework.CommonInterfaces;
+using BasicGameFramework.DIContainers;
+using BasicGameFramework.StandardImplementations.CrossPlatform.AutoresumeClasses;
+using CommonBasicStandardLibraries.Exceptions;
+namespace BasicGameFramework.StandardImplementations.CrossPlatform.MiscClasses
+{
+    public class SinglePlayerStartUpClass : IStartUp
+    {
+        void IStartUp.RegisterCustomClasses(GamePackageDIContainer container, bool multiplayer, BasicData data)
+        {
+            if (multiplayer == true)
+                throw new BasicBlankException("This can only be registered on single player classes");
+            //i think the only thing that needs to be registered is the autoresume classes alone.
+            container.RegisterType<SinglePlayerProductionSave>();
+        }
+        void IStartUp.StartVariables(BasicData data) { }
+    }
+}
